3aafacf29624cd2088a8244e79a24132d567cd32
Bernard <bernardkrasowski111@gmail.com>
2021-07-27 16:20:08 +0100

    connecting with backend

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -7,14 +7,38 @@ import LoginPage from './LoginPage';
 function AdminPage() {
 
   const [user, setUser] = useState(false)
+  const [login, setlogin] = useState('')
+  const [password, setpassword] = useState('')
+
+  const handlerInput = (e) => {
+    const txt = e.target.value;
+
+    if (e.target.type === "password") {
+      setpassword(txt);
+    } else {
+      setlogin(txt)
+    }
+
+  }
 
   const signIn = () => {
-    setUser(!user)
+    if (login != "" && password != "") {
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name: login, password: password })
+      };
+      fetch('http://localhost:9000/login', requestOptions)
+        .then(response => response.json())
+        .then(data => {
+          setUser(data)
+        })
+    } else { alert('Write login and password.') }
   }
   return (
     <>
       {!user ? (
-        <LoginPage sign={signIn} />
+        <LoginPage sign={signIn} log={login} pass={password} change={handlerInput} />
       )
         :
         (
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import '../styles/LoginPage.css'
 
-function LoginPage({ sign }) {
+function LoginPage({ sign, log, pass, change }) {
 
   return (
     <div className="wrapper__loginPage">
       <div className='loginPage'>
-        <input className='input__login' type="text" placeholder="Your Login" />
-        <input className='input__password' type="password" placeholder="Password" />
+        <input value={log} onChange={change} className='input__login' type="text" placeholder="Your Login" />
+        <input value={pass} onChange={change} className='input__password' type="password" placeholder="Password" />
         <button onClick={sign}>Sign in</button>
       </div>
     </div>
